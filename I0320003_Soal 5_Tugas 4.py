d97b21a32bbf2021ddfdab07e4cbffc743808d89
Afiq R <ramadhanendraputra@gmail.com>
2021-03-25 13:21:43 +0700

    Pengerjaan Soal 6

---
+++ I0320003_Soal 5_Tugas 4.py
@@ -0,0 +1,4 @@
+s = "Hey there! what shou"
+
+# panjangnya harusnya 20
+print("panjang dari s = ", len(s))
---
+++ I0320003_Soal 6_Tugas 4.py
@@ -0,0 +1,42 @@
+# Operator Bitwise
+
+a = 4       # 4 = 100
+b = 11      # 11 = 1011
+
+# bitwise OR (|)
+c = a | b
+print('\n===============OR=====================')
+print('nilai : ',a,' , binary :',format(a,'08b'))
+print('nilai : ',b,' , binary :',format(b,'08b'))
+print('------------------------------------ (|)')
+print('nilai : ',c,' , binary :',format(c,'08b'))
+
+# bitwise shift right (>>)
+c = a>>b
+print('\n===============>>=====================')
+print('nilai : ',a,' , binary :',format(a,'08b'))
+print('------------------------------------ (>>)')
+print('nilai : ',c,' , binary :',format(c,'08b'))
+
+# bitwise XOR (^)
+c = a ^ b
+print('\n===============XOR=====================')
+print('nilai : ',a,' , binary :',format(a,'08b'))
+print('nilai : ',b,' , binary :',format(b,'08b'))
+print('------------------------------------ (^)')
+print('nilai : ',c,' , binary :',format(c,'08b'))
+
+# bitwise NOT (~)
+c = ~ a
+print('\n===============NOT=====================')
+print('nilai : ',a,' , binary :',format(a,'08b'))
+print('------------------------------------ (~)')
+print('nilai : ',c,' , binary :',format(c,'08b'))
+
+# bitwise AND (&)
+c = a & b
+print('\n===============AND=====================')
+print('nilai : ',a,' , binary :',format(a,'08b'))
+print('nilai : ',b,' , binary :',format(b,'08b'))
+print('------------------------------------ (&)')
+print('nilai : ',c,' , binary :',format(c,'08b'))
